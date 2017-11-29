00f9fe1ba862e6b5d69adad560d91d9d7ee4a2b9
gsingharoy <neogauravsvnit@gmail.com>
2017-11-29 19:51:17 +0100

    Media Parser first commit
    Added a basic skeleton for the media-parser challenge

---
+++ media-parser/media_parser.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+class MediaParser
+
+  # Constructor to initialize the MediaParser
+  # @param filepath string path of the file's location which needs to be parsed
+  def initialize(filepath)
+    fail('implement me ;)')
+  end
+
+  # method to return a parsed hash summing up all the space each media type has taken
+  # @return Hash of the format { :video => <int>, :audio => <int>, :image => <int>, :other => <int> }
+  def parse
+    fail('you really need to implement me ;)')
+  end
+end
